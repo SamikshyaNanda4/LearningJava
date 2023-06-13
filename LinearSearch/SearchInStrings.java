34d567dde9be12829abfb2dc4caa9c3cf8ae7c45
Samikshya Nanda <91324968+SamikshyaNanda4@users.noreply.github.com>
2023-06-13 12:25:21 -0700

    -added Search in strings
    got to know that System.out.println(Arrays.toStrings(name.CharAt()));  gives an Array of char from a String to iterate upon .

---
+++ LinearSearch/SearchInStrings.java
@@ -0,0 +1,22 @@
+public class SearchInStrings {
+    public static void main(String[] args) {
+        String name = "Kunal";
+        char target = 'a';
+        System.out.println(search(name,target));
+        System.out.println(target);
+    }
+    static boolean search(String str ,char target){
+
+        if(str.length() == 0){
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if(target == str.charAt(i)){
+                return true;
+            }
+
+        }
+        return false;
+
+    }
+}
